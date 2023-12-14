aef3be2341284f42cbb0a63c6d6fa8e4b69fab5e
Tomas Gear <tomas@cabana.life>
2023-12-13 22:35:56 -0300

    update algorithm to fix counting, although its still inaccurate

--- countJuggle6.py
+++ countJuggle6.py
@@ -11,12 +11,9 @@ class CountJuggles:
         self.cap = cv2.VideoCapture(video_path)
 
         self.body_index = {"left_ankle": 15, "right_ankle": 16}
-
         self.prev_ball_position = None
         self.juggle_count = 0
-
-        self.proximity_threshold = 50 # pixels
-
+        self.proximity_threshold = 50  
         self.frame_skip = 2
         self.frame_counter = 0
 
@@ -28,7 +25,6 @@ class CountJuggles:
 
                 if self.frame_counter >= self.frame_skip:
                     self.frame_counter = 0
-
                     ball_position = None
 
                     pose_results = self.pose_model(frame, verbose=False, conf=0.5)
@@ -39,21 +35,17 @@ class CountJuggles:
                         for bbox in results.boxes.xyxy:
                             x1, y1, x2, y2 = bbox[:4]
                             ball_position = (x1 + x2) / 2, (y1 + y2) / 2
-
                             cv2.rectangle(pose_annotated_frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
 
                     keypoints_data = pose_results[0].keypoints.data
-
                     if keypoints_data.shape[1] > max(self.body_index.values()):
                         left_ankle = keypoints_data[0, self.body_index["left_ankle"], :2]
                         right_ankle = keypoints_data[0, self.body_index["right_ankle"], :2]
 
                         if ball_position is not None:
                             self.update_juggle_count(ball_position, left_ankle, right_ankle)
-                            self.prev_ball_position = ball_position
 
                     cv2.imshow("YOLOv8 Inference", pose_annotated_frame)
-
                     if cv2.waitKey(1) & 0xFF == ord("q"):
                         break
             else:
@@ -62,30 +54,16 @@ class CountJuggles:
         self.cap.release()
         cv2.destroyAllWindows()
 
-
     def update_juggle_count(self, ball_position, left_ankle, right_ankle):
-        if self.prev_ball_position is not None:
-            # Check if the ball is moving downwards
-            if ball_position[1] < self.prev_ball_position[1]:
-                # Calculate distance to both ankles
-                distance_to_left = np.linalg.norm(np.array(left_ankle) - np.array(ball_position))
-                distance_to_right = np.linalg.norm(np.array(right_ankle) - np.array(ball_position))
+        # Calculate distance to both ankles
+        distance_to_left = np.linalg.norm(np.array(left_ankle) - np.array(ball_position))
+        distance_to_right = np.linalg.norm(np.array(right_ankle) - np.array(ball_position))
 
-                print('Distance to left ankle: ', distance_to_left)
-                print('Distance to right ankle: ', distance_to_right)
-                print('Distance ball_position: ', ball_position)
+        # Check if the ball is close to either ankle
+        if distance_to_left < self.proximity_threshold or distance_to_right < self.proximity_threshold:
+            self.juggle_count += 1
 
-                # Check if the ball is close to either ankle
-                if distance_to_left < self.proximity_threshold or distance_to_right < self.proximity_threshold:
-                    self.juggle_count += 1
-
-            print(f"Juggle Count: {self.juggle_count}")
-
-    # def update_juggle_count(self, ball_position):
-    #     if self.prev_ball_position is not None:
-    #         if ball_position[1] < self.prev_ball_position[1]:
-    #             self.juggle_count += 1
-    #         print(f"Juggle Count: {self.juggle_count}")
+        print(f"Juggle Count: {self.juggle_count}")
 
 if __name__ == "__main__":
     count_juggles = CountJuggles()
